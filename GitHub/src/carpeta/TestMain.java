5188c91a9a15b59f906a796009124376da2746b2
javaee <javaee@JAVAeeVM>
2022-04-27 00:02:55 +0200

    Inicio de proyecto

---
+++ GitHub/src/carpeta/TestMain.java
@@ -0,0 +1,29 @@
+package carpeta;
+
+import javax.swing.JOptionPane;
+
+
+public class TestMain {
+
+   
+    public static void main(String[] args) {
+        
+        String texto=JOptionPane.showInputDialog("Introduce un tamaño para el array");
+        int tamanio=Integer.parseInt(texto);
+  
+        texto=JOptionPane.showInputDialog("Introduce la longitud del password");
+        int longitud=Integer.parseInt(texto);
+  
+        
+        Password listaPassword[]=new Password[tamanio];
+        boolean fortalezaPassword[]=new boolean[tamanio];
+  
+  
+        for(int i=0;i<listaPassword.length;i++){
+            listaPassword[i]=new Password(longitud);
+            fortalezaPassword[i]=listaPassword[i].esFuerte();
+            System.out.println(listaPassword[i].getContraseña()+" "+fortalezaPassword[i]);
+    }
+    
+}
+}
